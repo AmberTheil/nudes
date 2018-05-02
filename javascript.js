0b12d50444544f5f71d0e9c6e39984a67128c29e
Amber Theil <ambertheil@Ambers-MacBook-Pro.local>
2018-05-02 20:45:36 +0200

    hehe

--- javascript.js
+++ javascript.js
@@ -11,8 +11,24 @@ function sidenVises() {
     $("#hjerte2").hide();
     $("#4").hide();
     $("#chokface").hide();
+    $("#chokface1").hide();
+    $("#chokface2").hide();
+    $("#chokface3").hide();
+    $("#chokface4").hide();
+    $("#chokface5").hide();
+    $("#chokface6").hide();
     $("#iphone1").hide();
     $("#iphone2").hide();
+    $("#cry").hide();
+    $("#politisur").hide();
+    $("#polititext").hide();
+    $("#sweet")[0].play();
+    $("#sweet")[0].volume = .5;
+}
+
+function reset() {
+    console.log("Restart alt");
+    location.reload();
 }
 
 function start() {
@@ -36,9 +52,11 @@ function kiss() {
     $("#moerke").addClass("dark_scene");
     $("#kissing2").show();
     $("#hjerte2").show();
-
+    $("#kiss")[0].play();
+    $("#kiss")[0].currentTime = 5;
 
     setTimeout(senere, 7000);
+
 }
 
 function senere() {
@@ -68,12 +86,97 @@ function Valg() {
 
 
 function sendt() {
-    console.log("beskeden er sendt")
+    console.log("beskeden er sendt");
 
+    $("#iphone1").hide();
+    $("#send").hide();
+    $("#sendikke").hide();
+    $("#iphone2").show();
+
+    setTimeout(chokface1, 5000);
+}
+
+function chokface1() {
+    console.log("chockende ansigt1");
+
+    $("#iphone2").hide();
+    $("#chokface1").show();
+
+    setTimeout(chokface2, 0500);
+}
+
+function chokface2() {
+    console.log("chockende ansigt2");
+
+    $("#chokface2").show();
+
+    setTimeout(chokface3, 0500);
+}
+
+function chokface3() {
+    console.log("chockende ansigt3");
+
+    $("#chokface3").show();
+
+    setTimeout(chokface4, 0500);
+}
+
+function chokface4() {
+    console.log("chockende ansigt4");
+
+    $("#chokface4").show();
+
+    setTimeout(chokface5, 0500);
+}
+
+function chokface5() {
+    console.log("chockende ansigt5");
+
+    $("#chokface5").show();
+    setTimeout(chokface6, 0500);
+}
+
+function chokface6() {
+    console.log("chockende ansigt6");
+
+    $("#chokface6").show();
+    setTimeout(cry, 3000);
+}
+
+function cry() {
+    console.log("græder");
+
+    $("#cry").show();
+    setTimeout(politi, 7000);
+}
+
+function politi() {
+    console.log("politi")
+
+    $("#chokface1").hide();
+    $("#chokface2").hide();
+    $("#chokface3").hide();
+    $("#chokface4").hide();
+    $("#chokface5").hide();
+    $("#chokface6").hide();
+    $("#cry").hide();
+    $("#politisur").show();
+
+    $("#reset_sprite").on("click", reset);
 
 }
 
+
 function ikkeSendt() {
     console.log("beskeden er ikke sendt")
+
+    $("#iphone1").hide();
+    $("#send").hide();
+    $("#sendikke").hide();
+
+    $("#polititext").show();
+
+    $("#reset_sprite").on("click", reset);
+
 
 }
